46ceafafe8486809da9747ad7749444af763c31a
DamonLee5 <liwr5damon@gmail.com>
2020-03-25 21:55:54 -0400

    Update readBinaryFNames.m

--- code/ConeBeam/Modular_MatlabRoutines/readBinaryFNames.m
+++ code/ConeBeam/Modular_MatlabRoutines/readBinaryFNames.m
@@ -13,17 +13,22 @@ masterField = 'binaryFNames';
 value = '';
 resolveFlag = '-r';
 
+
 binaryFNames.sino = plainParams(executablePath, get_set, masterFile, masterField, 'sino', value, resolveFlag);
 
-binaryFNames.driftSino = plainParams(executablePath, get_set, masterFile, masterField, 'driftSino', value, resolveFlag);
+binaryFNames.estimateSino = plainParams(executablePath, get_set, masterFile, masterField, 'estimateSino', value, resolveFlag);
 
-binaryFNames.origSino = plainParams(executablePath, get_set, masterFile, masterField, 'origSino', value, resolveFlag);
+binaryFNames.errSino = plainParams(executablePath, get_set, masterFile, masterField, 'errSino', value, resolveFlag);
 
 binaryFNames.wght = plainParams(executablePath, get_set, masterFile, masterField, 'wght', value, resolveFlag);
 
-binaryFNames.errsino = plainParams(executablePath, get_set, masterFile, masterField, 'errsino', value, resolveFlag);
+binaryFNames.origSino = plainParams(executablePath, get_set, masterFile, masterField, 'origSino', value, resolveFlag);
 
-binaryFNames.sinoMask = plainParams(executablePath, get_set, masterFile, masterField, 'sinoMask', value, resolveFlag);
+binaryFNames.jigMeasurementsSino = plainParams(executablePath, get_set, masterFile, masterField, 'jigMeasurementsSino', value, resolveFlag);
+
+binaryFNames.driftSino = plainParams(executablePath, get_set, masterFile, masterField, 'driftSino', value, resolveFlag);
+
+binaryFNames.projOutput = plainParams(executablePath, get_set, masterFile, masterField, 'projOutput', value, resolveFlag);
 
 binaryFNames.recon = plainParams(executablePath, get_set, masterFile, masterField, 'recon', value, resolveFlag);
 
@@ -33,22 +38,21 @@ binaryFNames.proxMapInput = plainParams(executablePath, get_set, masterFile, mas
 
 binaryFNames.lastChange = plainParams(executablePath, get_set, masterFile, masterField, 'lastChange', value, resolveFlag);
 
-binaryFNames.timeToChange = plainParams(executablePath, get_set, masterFile, masterField, 'timeToChange', value, resolveFlag);
-
 binaryFNames.phantom = plainParams(executablePath, get_set, masterFile, masterField, 'phantom', value, resolveFlag);
-
-binaryFNames.sysMatrix = plainParams(executablePath, get_set, masterFile, masterField, 'sysMatrix', value, resolveFlag);
 
 binaryFNames.wghtRecon = plainParams(executablePath, get_set, masterFile, masterField, 'wghtRecon', value, resolveFlag);
 
+binaryFNames.projInput = plainParams(executablePath, get_set, masterFile, masterField, 'projInput', value, resolveFlag);
 
+binaryFNames.consensusRecon = plainParams(executablePath, get_set, masterFile, masterField, 'consensusRecon', value, resolveFlag);
+
+binaryFNames.sysMatrix = plainParams(executablePath, get_set, masterFile, masterField, 'sysMatrix', value, resolveFlag);
+
+binaryFNames.timeToChange = plainParams(executablePath, get_set, masterFile, masterField, 'timeToChange', value, resolveFlag);
+
+binaryFNames.backprojlikeInput = plainParams(executablePath, get_set, masterFile, masterField, 'backprojlikeInput', value, resolveFlag);
+
+binaryFNames.backprojlikeOutput = plainParams(executablePath, get_set, masterFile, masterField, 'backprojlikeOutput', value, resolveFlag);
 
 
 end
-
-
-
-
-
-
-
