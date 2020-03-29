ac6fda43eec54a7c282c056c67bf14afe525eba7
DamonLee5 <liwr5damon@gmail.com>
2020-03-29 02:34:33 -0400

    Update run file in Compare images

--- code/ConvertFiles/Compare_images/run_sm.m
+++ code/ConvertFiles/Compare_images/run_sm.m
@@ -1,37 +1,62 @@
-saveFolder = '/Volumes/Data/Cone_beam_results/Meeting_20181128/01_figures_paper/yzView_rightSection_016_crossSection';
+
+addpath(genpath('../../Modular_MatlabRoutines/'));
+addpath(genpath('../MatlabRoutines/'));
+
+saveFolder = '~/Desktop/crossSectionPlots';
 
 fNames = {};
-fNames{1} = '/Volumes/Data/Cone_beam_results/Meeting_20181128/01_figures_paper/yzView_rightSection_016_crossSection/qggmrf_4D.tif';
-fNames{2} = '/Volumes/Data/Cone_beam_results/Meeting_20181128/01_figures_paper/yzView_rightSection_016_crossSection/all.tif';
+i=0;
+i = i+1; fNames{i} = '/Users/smajee/Dropbox/Temp/phantom_4D_k=0_t=4_object.phantom.recon0005.png';
+i = i+1; fNames{i} = '/Users/smajee/Dropbox/Temp/FBP_4D_k=0_t=4_object.recon0005.png';
+i = i+1; fNames{i} = '/Users/smajee/Dropbox/Temp/MRF4D_z_cent_prior_t=5.recon0005.png';
+i = i+1; fNames{i} = '/Users/smajee/Dropbox/Temp/MSF_z_cent_prior_t=5.recon0005.png';
 
-titles = {};
-% [~, titles{1}] = fileparts(fNames{1});
-% [~, titles{2}] = fileparts(fNames{2});
-titles{1} = 'qggmrf 4D';
-titles{2} = 'our';
+imgNames = {};
+i=0;
+i = i+1; imgNames{i} = 'Phantom'
+i = i+1; imgNames{i} = 'FBP (3D)';
+i = i+1; imgNames{i} = 'MBIR+4D-MRF';
+i = i+1; imgNames{i} = 'Multi-Slice Fusion';
 
-titles_all = 'Compare Cross Section';
-
-magnification = 3;
-
-colors = {};
-colors{1} = 'b';
-colors{2} = 'r';
-colors{3} = 'g';
-colors{4} = 'c';
+colors{1} = 'k';
+colors{2} = 'g';
+colors{3} = 'r';
+colors{4} = 'b';
 colors{5} = 'm';
 colors{6} = 'k';
 
-lines = {};
 lines{1} = '-d';
 lines{2} = '-*';
+lines{3} = '-s';
+lines{4} = '-o';
 
-xi = [10, 20];
-yi = [100, 110];
-
-isProfile = true;
-isCrop = true;
+fullName = 'Compare Cross Section';
+isCrop = false;
+isNormalizeByFirst = true;
 isreducedMargin = true;
+isProfile = true;
+magnification = 3;
+LineWidth = 2;
 
-imageCompare(saveFolder, fNames, titles, titles_all, isProfile, xi, yi, colors, lines, isCrop, magnification, isreducedMargin);
+params.saveFolder = saveFolder;
+params.fNames = fNames;
+params.imgNames = imgNames;
+params.fullName = fullName;
+params.isCrop = isCrop;
+params.isNormalizeByFirst = isNormalizeByFirst;
+params.isreducedMargin = isreducedMargin;
+params.isProfile = isProfile;
+params.magnification = magnification;
+params.colors = colors;
+params.lines = lines;
+params.LineWidth = LineWidth;
+
+params
+
+createFolder_purge(saveFolder);
+
+[profile_list, ~] = imageCompare(params);
+
+
+
 
