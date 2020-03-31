1d96573876a89e5e36e2b2ce2691d96a2adf9158
Soumendu Majee <soumendu.fb9@gmail.com>
2020-03-30 23:20:37 -0400

    cleans volume render run file

--- code/View/VolumeRenderRecon/run.m
+++ code/View/VolumeRenderRecon/run.m
@@ -29,34 +29,6 @@ opts.BackgroundColor = 'k';
 opts.folderSuffix = 'view_4D';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Dataset Specific Params
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% kwikpen-static
-% opts.mode = 'absolute 0.05 0.3';
-% opts.range1 = '215:385';
-% opts.range2 = '135:305';
-% opts.range3 = '180:315';
-
-% kwikpen-dynamic
-% opts.mode = 'absolute 0.05 0.3';
-% opts.range1 = '90:200';
-% opts.range2 = '90:200';
-% opts.range3 = '75:190';
-
-% vial
-% opts.mode = 'absolute 0 0.1';
-% opts.flipVect = [0 0 0];
-% opts.transparencyLims = [0.05 0.1];
-% % opts.range1 = '60:540';
-% % opts.range2 = '70:580';
-% % opts.range3 = '1:end';
-% opts.CameraPosition = [0.0 1.0 0.32];
-
-% reverse eng
-opts.Isovalue = 0.2;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % volRender_reconList(fNameList, opts);
 surfRender_reconList(fNameList, opts);
