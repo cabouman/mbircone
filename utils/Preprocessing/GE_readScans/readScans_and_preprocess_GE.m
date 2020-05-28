e7a375f98ad2cb49b8d78de4daa203108e6c3155
Soumendu Majee <soumendu.fb9@gmail.com>
2020-05-28 17:01:21 -0400

    cleans up scanner specific preprocessing modules

--- utils/Preprocessing/GE_readScans/readScans_and_preprocess_GE.m
+++
@@ -1,8 +0,0 @@
-function [ ] = readScans_and_preprocess_GE( masterFile, plainParamsFile )
-
-data = readScans_GE(masterFile, plainParamsFile );
-
-preprocess(data, masterFile, plainParamsFile );
-
-end
-
--- utils/Preprocessing/LillyNSI_readScans/LillyNSI_temp.m
+++
@@ -1,5 +0,0 @@
-
-masterPathNamesFile = '../../Inversion/Control/master.txt';
-plainParamsFile = '/scratch/rice/s/smajee/cone_beam_project/Results/src/plainParams/plainParams.sh';
-
-readScans_and_preprocess_LillyNSI( masterPathNamesFile, plainParamsFile );
