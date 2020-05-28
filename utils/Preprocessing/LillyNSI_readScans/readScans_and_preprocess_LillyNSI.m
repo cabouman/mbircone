2e53c1f66a18a2e779a788c06058060d509310aa
Soumendu Majee <soumendu.fb9@gmail.com>
2020-05-28 17:01:21 -0400

    cleans up scanner specific preprocessing modules

--- utils/Preprocessing/LillyNSI_readScans/readScans_and_preprocess_LillyNSI.m
+++
@@ -1,7 +0,0 @@
-function [ ] = readScans_and_preprocess_LillyNSI( masterFile, plainParamsFile )
-
-data = readScans_LillyNSI(masterFile, plainParamsFile );
-
-preprocess(data, masterFile, plainParamsFile );
-
-end
