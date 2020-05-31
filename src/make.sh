a93cdcaaff72a7fbf10013983c839d57ce43c6e9
DamonLee5 <liwr5damon@gmail.com>
2020-05-31 17:46:33 -0400

    Move plainParams.c and .h to src

--- src/make.sh
+++ src/make.sh
@@ -38,8 +38,8 @@ fi
 mode="${1}"
 
 
-SOURCES="allocate.c MBIRModularUtilities3D.c io3d.c computeSysMatrix.c icd3d.c recon3DCone.c main.c"
-OBJECTS="allocate.o MBIRModularUtilities3D.o io3d.o computeSysMatrix.o icd3d.o recon3DCone.o main.o"
+SOURCES="allocate.c MBIRModularUtilities3D.c io3d.c computeSysMatrix.c icd3d.c recon3DCone.c main.c plainParams.c"
+OBJECTS="allocate.o MBIRModularUtilities3D.o io3d.o computeSysMatrix.o icd3d.o recon3DCone.o main.o plainParams.o"
 EXECUTABLE="main"
 
 
@@ -61,13 +61,13 @@ elif [[ "${mode}" = "all" ]]; then
     if [[  $STATUS != 0 ]]; then generalError "$0 $@"; exit 1; fi
 
     set -x
-        icc -fopenmp -O3 -Wall -pedantic ${OBJECTS} ../utils/plainParams/plainParams.o -o ${EXECUTABLE}
+        icc -fopenmp -O3 -Wall -pedantic ${OBJECTS}  -o ${EXECUTABLE}
     { STATUS=$?; set +x; } 2>/dev/null
     if [[  $STATUS != 0 ]]; then generalError "$0 $@"; exit 1; fi
 
     echo put all object and executables in the bin
     mv ${OBJECTS} ${EXECUTABLE} ../bin
-    mv ../utils/plainParams/plainParams.o ../bin
+    
 
 # -------- Help --------------------------------------------
 
@@ -77,7 +77,7 @@ elif [[ "${mode}" = "clean" ]]; then
     cd "${scriptDir}"
     rm ${OBJECTS} ${EXECUTABLE}
     cd ../bin
-    rm ${OBJECTS} ${EXECUTABLE} plainParams.o
+    rm ${OBJECTS} ${EXECUTABLE} 
 
 
 
