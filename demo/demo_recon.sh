58b33ad3766d6e59120d06c13ec30b0f2e8e9334
Soumendu Majee <soumendu.fb9@gmail.com>
2020-06-23 12:53:01 -0400

    seperats reconstruction demo from preprocessing

---
+++ demo/demo_recon.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# uncomment the following line to override mac's readlink with GNU readlink (more info in readme)
+# alias readlink='greadlink'
+
+cur=$(pwd)
+cd ..
+make
+cd ${cur}
+
+master=$(readlink -f "params/master.txt")
+
+bash .././ConeBeam.sh "${master}" CBMODE_INV_prepare
+
+bash .././ConeBeam.sh "${master}" CBMODE_INV_recon
+
