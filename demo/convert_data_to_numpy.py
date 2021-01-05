dda2efc77868ea8bd1849f2cf084e0f4d6471f2e
Soumendu Majee <soumendu.fb9@gmail.com>
2021-01-05 18:11:01 -0500

    convert data into numpy

--- demo/convert_data_to_numpy.py
+++ demo/convert_data_to_numpy.py
@@ -4,5 +4,10 @@ from utils import *
 
 x = read_recon3D('object.sino')
 x = np.copy(np.swapaxes(x, 0, 2), order='C')
-
 print(x.shape)
+np.save('sino.npy', x)
+
+x = read_recon3D('object.wght')
+x = np.copy(np.swapaxes(x, 0, 2), order='C')
+print(x.shape)
+np.save('wght.npy', x)
