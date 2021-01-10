9dcf1802d160e0885b08a04f01a2dcc2d6b2e936
Soumendu Majee <soumendu.fb9@gmail.com>
2021-01-09 20:22:15 -0500

    reorganize writesysmatrix routine

--- mbircone/src/cyInterface.c
+++ mbircone/src/cyInterface.c
@@ -2,42 +2,19 @@
 #include "allocate.h"
 #include "interface.h"
 
-int interface_matrix_multiplication(struct flat_array_2D *A_flat, struct flat_array_2D *B_flat , struct flat_array_2D *C_flat)
+
+int writeSysMatrix(char *fName, double *angles, struct SinoParams *sinoParams, struct ImageParams *imgParams)
 {
+    struct SysMatrix A;
+    struct ViewAngleList viewAngleList;
 
-	struct matrix_float A,B,C;
-
-	/* Allocate 2D multi-alloc pointer array from cython flattened array */
-    allocate_3darray_from_flattened(&A, A_flat);
-    allocate_3darray_from_flattened(&B, B_flat);
-    allocate_3darray_from_flattened(&C, C_flat);
-
-    /* Original c code for matrix multiplication that uses multi-alloc array*/
-
-    /* Deallocate multi-alloc pointer array */
-    deallocate_3darray_to_flattened(&A);
-    deallocate_3darray_to_flattened(&B);
-    deallocate_3darray_to_flattened(&C);
-
-    return 0;
-}
-
-/*
-int writeSysMatrix(char *fName, struct SinoParams *sinoParams, struct ImageParams *imgParams)
-{
-    readAndAllocateViewAngleList(pathNames.masterFile, pathNames.plainParamsFile, &viewAngleList, &sino.params);
-
-    computeSysMatrix(&sino.params, &img.params, &A, &viewAngleList);
-    isExistSysMatrix = 1;
+    computeSysMatrix(sinoParams, &imgParams, &A, &viewAngleList);
     
     printSysMatrixParams(&A);
     writeSysMatrix(pathNames.sysMatrix, &sino.params, &img.params, &A);
 
-    freeViewAngleList(&viewAngleList);
-
     return 0;
-}*/
-
+}
 
 
 void allocate_3darray_from_flattened(struct matrix_float *A, struct flat_array_2D *array)
