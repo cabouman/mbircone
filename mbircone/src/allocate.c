9410e26477389889ab77d5ee13849e7cd657c75e
Soumendu Majee <soumendu.fb9@gmail.com>
2021-02-08 20:45:34 -0500

    reorder include statements to make more robust

--- mbircone/src/allocate.c
+++ mbircone/src/allocate.c
@@ -29,8 +29,8 @@
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ============================================================================== */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
 
