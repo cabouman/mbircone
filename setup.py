4649f2b02ac13f6c0e7573c1e48daffb20c9f6ec
Soumendu Majee <soumendu.fb9@gmail.com>
2021-02-21 01:22:34 -0500

    purge plainparams and io dependencies from python installation

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ AUTHOR = "Soumendu Majee"
 
 # Specifies directory containing cython functions to be compiled
 PACKAGE_DIR = "mbircone"
-SRC_FILES = [PACKAGE_DIR+'/src/allocate.c', PACKAGE_DIR+'/src/computeSysMatrix.c',
-             PACKAGE_DIR+'/src/cyInterface.c', PACKAGE_DIR+'/src/icd3d.c', PACKAGE_DIR+'/src/io3d.c',
-             PACKAGE_DIR+'/src/MBIRModularUtilities3D.c', PACKAGE_DIR+'/src/recon3DCone.c', PACKAGE_DIR+'/src/plainParams.c',
-             PACKAGE_DIR+'/conebeam.pyx']
+SRC_FILES = [PACKAGE_DIR+'/src/allocate.c', PACKAGE_DIR+'/src/MBIRModularUtilities3D.c',
+             PACKAGE_DIR+'/src/icd3d.c', PACKAGE_DIR+'/src/recon3DCone.c',
+             PACKAGE_DIR+'/src/computeSysMatrix.c',
+             PACKAGE_DIR+'/src/cyInterface.c', PACKAGE_DIR+'/conebeam.pyx']
 
 
 compiler_str = os.environ.get('CC')
