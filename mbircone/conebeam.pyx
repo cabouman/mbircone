c9330440a07190d572491b655a123e197c5c7319
DamonLee5 <liwr5damon@gmail.com>
2021-02-23 22:03:22 -0500

    Add reconparams.

--- mbircone/conebeam.pyx
+++ mbircone/conebeam.pyx
@@ -3,6 +3,8 @@ import numpy as np
 import ctypes           # Import python package required to use cython
 cimport cython          # Import cython package
 cimport numpy as cnp    # Import specialized cython support for numpy
+from libc.string cimport strncpy
+
 
 # Import c data structure
 cdef extern from "./src/MBIRModularUtilities3D.h":
@@ -51,10 +53,77 @@ cdef extern from "./src/MBIRModularUtilities3D.h":
         long int N_z_roi;
 
 
+
+    struct ReconParams:
+        double InitVal_recon;                  # Initialization value InitVal_proxMapInput (mm-1) 
+        char initReconMode[200];
+    
+        double priorWeight_QGGMRF;                  # Prior mode: (0: off, 1: QGGMRF, 2: proximal mapping) 
+        double priorWeight_proxMap;                  # Prior mode: (0: off, 1: QGGMRF, 2: proximal mapping) 
+        
+        # QGGMRF 
+        double q;                   # q: QGGMRF parameter (q>1, typical choice q=2) 
+        double p;                   # p: QGGMRF parameter (1<=p<q) 
+        double T;                   # T: QGGMRF parameter 
+        double sigmaX;              # sigmaX: QGGMRF parameter 
+        double bFace;               # bFace: relative neighbor weight: cube faces 
+        double bEdge;               # bEdge: relative neighbor weight: cube edges 
+        double bVertex;             # bVertex: relative neighbor weight: cube vertices 
+        # Proximal Mapping 
+        double sigma_lambda;        # sigma_lambda: Proximal mapping scalar 
+        int is_positivity_constraint;
+        
+
+         # Stopping Conditions
+
+        double stopThresholdChange_pct;           # stop threshold (%) 
+        double stopThesholdRWFE_pct;
+        double stopThesholdRUFE_pct;
+        int MaxIterations;              # maximum number of iterations 
+        char relativeChangeMode[200];
+        double relativeChangeScaler;
+        double relativeChangePercentile;
+    
+    
+         # Zipline Stuff
+
+        int N_G;                # Number of groups for group ICD 
+        int zipLineMode;                # Zipline mode: (0: off, 1: conventional Zipline, 2: randomized Zipline) 
+        int numVoxelsPerZiplineMax;
+        int numVoxelsPerZipline;
+        int numZiplines;
+    
+
+        # Parallel Stuff
+        int numThreads;                 # numThreads: Number of threads 
+    
+        # Weight scaler stuff
+
+        char weightScaler_estimateMode[200];     # Estimate weight scaler? 1: Yes. 0: Use user specified value 
+        char weightScaler_domain[200];     
+        double weightScaler_value;            # User specified weight scaler 
+    
+    
+        # NHICD stuff 
+        char NHICD_Mode[200];
+        double NHICD_ThresholdAllVoxels_ErrorPercent;
+        double NHICD_percentage;
+        double NHICD_random;
+    
+        # Misc 
+        int verbosity;
+        int isComputeCost;
+        char backprojlike_type[200]; 
+
+
 # Import a c function to compute A matrix.
 cdef extern from "./src/cyInterface.h":
     void AmatrixComputeToFile(double *angles, SinoParams c_sinoparams, ImageParams c_imgparams, 
         char *Amatrix_fname, char verbose);
+
+    void recon(float *x, float *sino, float *wght, float *x_init, float *proxmap_input,
+	SinoParams c_sinoparams, ImageParams c_imgparams, ReconParams c_reconParams,
+	char *Amatrix_fname);
 
 
 cdef map_py2c_sinoparams(SinoParams* c_sinoparams, sinoparams):
@@ -92,6 +161,70 @@ cdef map_py2c_imgparams(ImageParams* c_imgparams, imgparams):
     c_imgparams.N_x_roi = imgparams['N_x_roi']
     c_imgparams.N_y_roi = imgparams['N_y_roi']
     c_imgparams.N_z_roi = imgparams['N_z_roi']
+
+
+cdef map_py2c_reconparams(ReconParams* c_reconparams, reconparams):
+        c_reconparams.InitVal_recon = c_reconparams['InitVal_recon']                  # Initialization value InitVal_proxMapInput (mm-1)
+        char initReconMode[200];
+
+        c_reconparams.priorWeight_QGGMRF = c_reconparams['priorWeight_QGGMRF']                  # Prior mode: (0: off, 1: QGGMRF, 2: proximal mapping)
+        c_reconparams.priorWeight_proxMap = c_reconparams['priorWeight_proxMap']                  # Prior mode: (0: off, 1: QGGMRF, 2: proximal mapping)
+
+        # QGGMRF
+        c_reconparams.q = c_reconparams['q']                   # q: QGGMRF parameter (q>1, typical choice q=2)
+        c_reconparams.p = c_reconparams['p']                   # p: QGGMRF parameter (1<=p<q)
+        c_reconparams.T = c_reconparams['T']                   # T: QGGMRF parameter
+        c_reconparams.sigmaX = c_reconparams['sigmaX']              # sigmaX: QGGMRF parameter
+        c_reconparams.bFace = c_reconparams['bFace']               # bFace: relative neighbor weight: cube faces
+        c_reconparams.bEdge = c_reconparams['bEdge']               # bEdge: relative neighbor weight: cube edges
+        c_reconparams.bVertex = c_reconparams['bVertex']             # bVertex: relative neighbor weight: cube vertices
+        # Proximal Mapping
+        c_reconparams.sigma_lambda = c_reconparams['sigma_lambda']        # sigma_lambda: Proximal mapping scalar
+        c_reconparams.is_positivity_constraint = c_reconparams['is_positivity_constraint']
+
+
+         # Stopping Conditions
+
+        c_reconparams.stopThresholdChange_pct = c_reconparams['stopThresholdChange_pct']           # stop threshold (%)
+        c_reconparams.stopThesholdRWFE_pct = c_reconparams['stopThesholdRWFE_pct']
+        c_reconparams.stopThesholdRUFE_pct = c_reconparams['stopThesholdRUFE_pct']
+        c_reconparams.MaxIterations = c_reconparams['MaxIterations']              # maximum number of iterations
+        char relativeChangeMode[200];
+        c_reconparams.relativeChangeScaler = c_reconparams['relativeChangeScaler']
+        c_reconparams.relativeChangePercentile = c_reconparams['relativeChangePercentile']
+
+
+         # Zipline Stuff
+
+        c_reconparams.N_G = c_reconparams['N_G']                # Number of groups for group ICD
+        c_reconparams.zipLineMode = c_reconparams['zipLineMode']                # Zipline mode: (0: off, 1: conventional Zipline, 2: randomized Zipline)
+        c_reconparams.numVoxelsPerZiplineMax = c_reconparams['numVoxelsPerZiplineMax']
+        c_reconparams.numVoxelsPerZipline = c_reconparams['numVoxelsPerZipline']
+        c_reconparams.numZiplines = c_reconparams['numZiplines']
+
+
+        # Parallel Stuff
+        c_reconparams.numThreads = c_reconparams['numThreads']                 # numThreads: Number of threads
+
+        # Weight scaler stuff
+
+        char weightScaler_estimateMode[200];     # Estimate weight scaler? 1: Yes. 0: Use user specified value
+        char weightScaler_domain[200];
+        c_reconparams.weightScaler_value = c_reconparams['weightScaler_value']            # User specified weight scaler
+
+
+        # NHICD stuff
+        char NHICD_Mode[200];
+        c_reconparams.NHICD_ThresholdAllVoxels_ErrorPercent = c_reconparams['NHICD_ThresholdAllVoxels_ErrorPercent']
+        c_reconparams.NHICD_percentage = c_reconparams['NHICD_percentage']
+        c_reconparams.NHICD_random = c_reconparams['NHICD_random']
+
+        # Misc
+        c_reconparams.verbosity = c_reconparams['verbosity']
+        c_reconparams.isComputeCost = c_reconparams['isComputeCost']
+        char backprojlike_type[200];
+
+
 
 def string_to_char_array(input_str):
     """
