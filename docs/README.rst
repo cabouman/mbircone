4b10fca4d720eefcbba26126ff91f50bab095727
Wenrui Li <li3120@ece-72-27.dhcp.ecn.purdue.edu>
2021-06-25 15:13:26 -0400

    Add docs folder for sphnix auto documentation

---
+++ docs/README.rst
@@ -0,0 +1,34 @@
+**mbircone** stands for Model-Based Iterative Reconstruction for cone-beam.
+mbircone is an easy-to-use python package for fast cone-beam reconstruction of tomography data using model-based priors.
+
+
+Features
+--------
+* Easy-to-use python code for fast cone-beam MBIR (Model Based Iterative Reconstruction)
+
+* Supports MBIR reconstruction with Bayesian and Plug-and-Play prior models.
+
+
+System Requirements
+-------------------
+1. GCC compiler version 4.8.5 or above
+2. OpenMP Libraries
+3. Python>=3.6
+
+
+Optional System Requirements
+----------------------------
+Fastest reconstruction can be obtained with,
+
+* Intel-based CPU(s) supporting AVX2,AVX512
+* Intel ICC compiler (in "Parallel Studio XE", and now free "oneAPI")
+
+We also recommend:
+
+* Installation using conda environment
+
+License
+-------
+The project is licensed under the `BSD 3-Clause <https://github.com/cabouman/svmbir/blob/master/LICENSE>`_ License.
+
+
---
+++ docs/source/conf.py
@@ -0,0 +1,108 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import sphinx_rtd_theme
+
+if os.environ.get('MBIRCONE_BUILD_DOCS') =='true':
+    sys.path.insert(0, os.path.abspath('../..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'mbircone'
+copyright = '2021, MBIRCONE Development Team'
+author = 'MBIRCONE Development Team'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive',
+    'nbsphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.todo',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinxcontrib.bibtex',
+    'sphinx.ext.viewcode'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# -- Options for Napoleon -----------------------------------------------------
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = False
+napoleon_use_rtype = False
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+#html_theme = 'bizstyle'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme_options = {
+    'style_nav_header_background': '#4f8fb8ff',
+    'collapse_navigation': False,
+}
+
+# html_logo = 'mbircone_logo.svg'
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = project + 'doc'
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+#html_static_path = ['_static']
+html_static_path = []
