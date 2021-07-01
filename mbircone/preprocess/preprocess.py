641bbc7097edeb5cc7012bde3b2747463f21d8f7
DamonLee5 <liwr5damon@gmail.com>
2021-06-30 23:15:59 -0400

    Add tests to test downsample and crop.

--- mbircone/preprocess/preprocess.py
+++ mbircone/preprocess/preprocess.py
@@ -3,7 +3,6 @@ import os
 from glob import glob
 import numpy as np
 from PIL import Image
-from skimage.measure import block_reduce
 import matplotlib.pyplot as plt
 import warnings
 
@@ -78,7 +77,8 @@ def read_scans_all(obj_scan_dir, blank_scan_dir='', dark_scan_dir='', view_ids=N
     return obj_scan, blank_scan, dark_scan
 
 
-def downsample_scans(obj_scan, blank_scan, dark_scan, factor=1):
+
+def downsample_scans(obj_scan, blank_scan, dark_scan, factor=[1,1]):
 
     assert len(factor)==2, 'factor({}) needs to be of len 2'.format(factor)
 
@@ -89,9 +89,9 @@ def downsample_scans(obj_scan, blank_scan, dark_scan, factor=1):
     blank_scan = blank_scan[:,0:new_size1,0:new_size2]
     dark_scan = dark_scan[:,0:new_size1,0:new_size2]
 
-    obj_scan = block_reduce(obj_scan, block_size=(1, factor[0], factor[1]), func=np.sum)
-    blank_scan = block_reduce(blank_scan, block_size=(1, factor[0], factor[1]), func=np.sum)
-    dark_scan = block_reduce(dark_scan, block_size=(1, factor[0], factor[1]), func=np.sum)
+    obj_scan = obj_scan.reshape(obj_scan.shape[0], obj_scan.shape[1]//factor[0], factor[0], obj_scan.shape[2]//factor[1], factor[1]).sum((2,4))
+    blank_scan = blank_scan.reshape(blank_scan.shape[0], blank_scan.shape[1]//factor[0], factor[0], blank_scan.shape[2]//factor[1], factor[1]).sum((2,4))
+    dark_scan = dark_scan.reshape(dark_scan.shape[0], dark_scan.shape[1]//factor[0], factor[0], dark_scan.shape[2]//factor[1], factor[1]).sum((2,4))
 
     return obj_scan, blank_scan, dark_scan
 
@@ -138,7 +138,7 @@ def compute_views_index_list(view_range, num_views):
 
     index_original = range(view_range[0], view_range[1]+1)
     assert num_views <= len(index_original), 'num_views cannot exceed range of view index'
-    index_sampled = [view_range[0]+np.int(np.floor(i*len(index_original)/num_views)) for i in range(num_views)]
+    index_sampled = [view_range[0]+int(np.floor(i*len(index_original)/num_views)) for i in range(num_views)]
     return index_sampled
 
 
@@ -156,11 +156,10 @@ def compute_angles_list( view_index_list, num_acquired_scans, total_angles,  rot
     return angles_list
 
 
-def preprocess(path_radiographs, path_blank='gain0.tif', path_dark='offset.tif',
+def preprocess(path_radiographs, path_blank, path_dark,
                view_range=[0,1999], angle_span=360, num_views=20, full_views=2000,
-               rotation_direction="positive", downsample_factor=[4,4]):
+               rotation_direction="positive", downsample_factor=[1,1], crop_factor=[[0,0],[1,1]]):
     view_ids = compute_views_index_list(view_range, num_views)
-    print(view_ids)
     angles = compute_angles_list(view_ids, full_views,angle_span, rotation_direction)
     obj_scan = read_scan_dir(path_radiographs, view_ids)
     if path_blank is not None:
@@ -169,11 +168,11 @@ def preprocess(path_radiographs, path_blank='gain0.tif', path_dark='offset.tif',
         dark_scan = np.expand_dims(read_scan_img(path_dark), axis = 0)
 
     # downsampling in views and pixels
-    # obj_scan, blank_scan, dark_scan = downsample_scans(obj_scan, blank_scan, dark_scan,
-    #                                     factor=downsample_factor)
-    # obj_scan, blank_scan, dark_scan = crop_scans(obj_scan, blank_scan, dark_scan,
-    #                                     limits_lo=dataset_params['crop_limits_lo'],
-    #                                     limits_hi=dataset_params['crop_limits_hi'])
+    obj_scan, blank_scan, dark_scan = downsample_scans(obj_scan, blank_scan, dark_scan,
+                                        factor=downsample_factor)
+    obj_scan, blank_scan, dark_scan = crop_scans(obj_scan, blank_scan, dark_scan,
+                                        limits_lo=crop_factor[0],
+                                        limits_hi=crop_factor[1])
 
     sino = compute_sino(obj_scan, blank_scan, dark_scan)
     return sino.astype(np.float32), angles.astype(np.float32)
@@ -182,33 +181,117 @@ def preprocess(path_radiographs, path_blank='gain0.tif', path_dark='offset.tif',
 def test_1():
     """
 
-    Test preprocessing without downsampling and cropping
+    Test preprocessing without downsampling and cropping.
+    Dataset: metal_weld_data
+
+    Test result：
+    NRMSE between sino and ref_sino: 0.0
+    Sampled angles list:
+    [0.         5.8025217  5.3187165  4.834911   4.3511057  3.8673005
+     3.3834953  2.9028316  2.4190264  1.9352211  1.4514158  0.96761054
+     0.48380527]
 
     """
+    # create output folder
+    print("Test 1")
+    os.makedirs('./test_1/ref', exist_ok=True)
+    os.makedirs('./test_1/output', exist_ok=True)
+
     dataset_dir = "/depot/bouman/users/li3120/datasets/metal_weld_data/"
     path_radiographs = dataset_dir+"Radiographs-2102414-2019-001-076/"
     sino, angles= preprocess(path_radiographs, path_blank=dataset_dir + 'Corrections/gain0.tif', path_dark=dataset_dir + 'Corrections/offset.tif',
                             view_range=[0, 1999], angle_span=360, num_views=13,rotation_direction="negative", downsample_factor=[1, 1])
-    ref_sino = read_ND("./metal_laser_welds_cmp/object.sino", 3)
+    ref_sino = read_ND("./test_1/ref/object.sino", 3)
     ref_sino = np.copy(np.swapaxes(ref_sino, 1, 2))
     ref_sino = np.flip(ref_sino,axis = 1)
-    np.save("sino.npy", sino)
-    print(np.shape(sino))
-    print(np.shape(ref_sino))
-    plt.imshow(ref_sino[0]-sino[0])
+    np.save("./test_1/output/sino.npy", sino)
+    plt.imshow(ref_sino[0]-sino[0],vmin = -0.00001, vmax = 0.00001)
     plt.colorbar()
-    plt.savefig('diff_sino.png')
+    plt.savefig('./test_1/output/diff_sino.png')
     plt.close()
-    print(nrmse(sino[0], ref_sino[0]))
+    print("NRMSE between sino and ref_sino:", nrmse(sino, ref_sino))
+    print("Sampled angles list:")
     print(angles)
 
+
+def test_2():
     """
+
+    Test preprocessing with downsampling and without cropping.
+    Dataset: metal_weld_data
+
     Test result：
-    (10, 1920, 1536)
-    (10, 1920, 1536)
-    0.0
+    NRMSE between sino and ref_sino: 5.904646e-07
+
     """
+    # create output folder
+    print("Test 2")
+    os.makedirs('./test_2/ref', exist_ok=True)
+    os.makedirs('./test_2/output', exist_ok=True)
+
+    dataset_dir = "/depot/bouman/users/li3120/datasets/metal_weld_data/"
+    path_radiographs = dataset_dir+"Radiographs-2102414-2019-001-076/"
+    sino, angles= preprocess(path_radiographs, path_blank=dataset_dir + 'Corrections/gain0.tif', path_dark=dataset_dir + 'Corrections/offset.tif',
+                            view_range=[0, 1999], angle_span=360, num_views=13,rotation_direction="negative", downsample_factor=[2, 2])
+    ref_sino = read_ND("./test_2/ref/object.sino", 3)
+    ref_sino = np.copy(np.swapaxes(ref_sino, 1, 2))
+    ref_sino = np.flip(ref_sino,axis = 1)
+    np.save("./test_2/output/sino.npy", sino)
+    plt.imshow(ref_sino[0]-sino[0],vmin = -0.00001, vmax = 0.00001)
+    plt.colorbar()
+    plt.savefig('./test_2/output/diff_sino.png')
+    plt.close()
+    plt.imshow(ref_sino[0])
+    plt.colorbar()
+    plt.savefig('./test_2/output/ref.png')
+    plt.close()
+    plt.imshow(sino[0])
+    plt.colorbar()
+    plt.savefig('./test_2/output/sino.png')
+    plt.close()
+    print("NRMSE between sino and ref_sino:", nrmse(sino, ref_sino))
+
+
+def test_3():
+    """
+
+    Test preprocessing with downsampling and cropping.
+    Dataset: metal_weld_data
+
+    Test result：
+    NRMSE between sino and ref_sino: 3.314937e-07
+
+    """
+    # create output folder
+    print("Test 3")
+    os.makedirs('./test_3/ref', exist_ok=True)
+    os.makedirs('./test_3/output', exist_ok=True)
+
+    dataset_dir = "/depot/bouman/users/li3120/datasets/metal_weld_data/"
+    path_radiographs = dataset_dir+"Radiographs-2102414-2019-001-076/"
+    sino, angles= preprocess(path_radiographs, path_blank=dataset_dir + 'Corrections/gain0.tif', path_dark=dataset_dir + 'Corrections/offset.tif',
+                            view_range=[0, 1999], angle_span=360, num_views=13,rotation_direction="negative", downsample_factor=[2, 2], crop_factor=[[0.25,0],[0.75,1]])
+    ref_sino = read_ND("./test_3/ref/object.sino", 3)
+    ref_sino = np.copy(np.swapaxes(ref_sino, 1, 2))
+    ref_sino = np.flip(ref_sino,axis = 1)
+    np.save("./test_3/output/sino.npy", sino)
+    plt.imshow(ref_sino[0]-sino[0],vmin = -0.00001, vmax = 0.00001)
+    plt.colorbar()
+    plt.savefig('./test_3/output/diff_sino.png')
+    plt.close()
+    plt.imshow(ref_sino[0])
+    plt.colorbar()
+    plt.savefig('./test_3/output/ref.png')
+    plt.close()
+    plt.imshow(sino[0])
+    plt.colorbar()
+    plt.savefig('./test_3/output/sino.png')
+    plt.close()
+    print("NRMSE between sino and ref_sino:", nrmse(sino, ref_sino))
+
 
 if __name__ == '__main__':
     test_1()
+    test_2()
+    test_3()
 
