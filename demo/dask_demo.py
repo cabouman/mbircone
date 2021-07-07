cb27a9992ac81b9d7ec793ed5bfd0820f287ed08
DamonLee5 <liwr5damon@gmail.com>
2021-07-06 22:55:38 -0400

    Bug: use np.minimum to compare 2 values. Implement mulitnode for 4D datasets.

---
+++ demo/dask_demo.py
@@ -0,0 +1,104 @@
+import time
+import logging
+import socket
+import os
+from pprint import pprint
+import mbircone
+from dask.distributed import Client, as_completed
+import numpy as np
+from dask_jobqueue import SLURMCluster
+
+# dask.config.set({"distributed.admin.tick.limit":'3h'})
+def mbircone_4d(t):
+    dataset_dir = "/depot/bouman/users/li3120/datasets/Kinetic_Vision_4D_CT_Data/"
+    path_radiographs = dataset_dir+"Radiographs-210414_Kinetic-Vision_Fixture-Vision_Dynamic-4D_Cap/"
+    num_views = 72000
+    downsample_factor=[2, 2]
+    crop_factor=[[0.05,0.05],[0.95,0.95]]
+    index_time_points = 15
+
+    NSI_system_params = mbircone.preprocess.read_NSI_params("/depot/bouman/users/li3120/datasets/Kinetic_Vision_4D_CT_Data/210414_Kinetic-Vision_Fixture-Vision_Dynamic-4D_Cap.nsipro")
+
+    sino, angles= mbircone.preprocess.preprocess(path_radiographs, 
+        path_blank=dataset_dir + 'Corrections/gain0.tif', 
+        path_dark=dataset_dir + 'Corrections/offset.tif',
+        view_range=[0, NSI_system_params["num_acquired_scans"]-1], 
+        total_angles=NSI_system_params["total_angles"], 
+        num_views=num_views, 
+        num_acquired_scans=NSI_system_params["num_acquired_scans"],
+        rotation_direction="negative", 
+        downsample_factor=downsample_factor, 
+        crop_factor=crop_factor,
+        num_time_points=NSI_system_params["total_angles"]//360,
+        index_time_points=index_time_points)
+
+    print("sinogram shape:", sino.shape)
+    print("Angles List:")
+    print(angles)
+
+
+    NSI_system_params=mbircone.preprocess.adjust_NSI_sysparam(NSI_system_params,
+        downsample_factor=downsample_factor, 
+        crop_factor=crop_factor)
+
+    geo_params = mbircone.preprocess.transfer_NSI_to_MBIRCONE(NSI_system_params)
+    print("NSI system paramemters:")
+    pprint(NSI_system_params)
+    print("MBIRCONE Geometric paramemters:")
+    pprint(geo_params)
+
+    dist_source_detector = geo_params["dist_source_detector"]
+    magnification = geo_params["magnification"]
+    delta_pixel_detector = geo_params["delta_pixel_detector"]
+    channel_offset = geo_params["channel_offset"]
+    row_offset = geo_params["row_offset"]
+
+
+
+    x = mbircone.cone3D.recon(sino, angles, dist_source_detector=dist_source_detector, magnification=magnification, 
+        delta_pixel_detector=delta_pixel_detector,
+        channel_offset=channel_offset, row_offset=row_offset, weight_type='transmission', p=1.15, q=2.2, sharpness=1, num_neighbors=26,
+        max_iterations=20,lib_path='./output')
+
+    # create output folder
+    os.makedirs('output/kv4d/', exist_ok=True)
+
+    # fname_ref = '/depot-new/bouman/users/li3120/OpenMBIR-ConeBeam-4D/demo_3/data/conebeam/object.recon'
+    # ref = demo_utils.read_ND(fname_ref, 3)
+    # ref = np.swapaxes(ref, 0, 2)
+    # # ref = np.flip(ref,axis=0)
+    # rmse_val = demo_utils.nrmse(x,ref)
+    # print("NRMSE between reconstruction and reference: {}".format(rmse_val))
+
+    np.save("./output/kv4d/recon_t%d.npy"%index_time_points,x)    
+    return {'time point': t,
+            'host': socket.gethostname(),
+            'pid': os.getpid(),
+            'time': int(time.time() % 100)}
+
+
+cluster = SLURMCluster(project='standby',processes=2 ,n_workers=20,walltime='01:00:00', memory='16 GB',
+                       death_timeout=60, job_extra=['--nodes=20', '--ntasks-per-node=1'],
+                       env_extra=['module load anaconda',
+                                  'source activate mbircone'],
+                       cores=20)
+
+cluster.scale(jobs=3)
+print(cluster.job_script())
+client = Client(cluster)
+
+nb_workers = 0
+while True:
+    nb_workers = len(client.scheduler_info()["workers"])
+    print('Got {} workers'.format(nb_workers))
+    if nb_workers >= 2:
+        break
+    time.sleep(1)
+
+# futures = client.map(slow_increment, range(0,1000,5))
+
+print('client:', client)
+
+for future in as_completed(client.map(mbircone_4d, range(10,70,10))): # FIX
+    result = future.result()
+    pprint(result)
