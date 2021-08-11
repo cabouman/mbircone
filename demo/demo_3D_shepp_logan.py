2b5bd45796fdf56672e68dc2498938c0c4840b2d
Wenrui Li <li3120@ece-72-7.dhcp.ecn.purdue.edu>
2021-08-10 20:00:51 -0400

    Modify demo_3D_shepp_logan.py. Use more accurate titles for subplots.

--- demo/demo_3D_shepp_logan.py
+++ demo/demo_3D_shepp_logan.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import mbircone
-from demo_utils import plot_image, plot_gif
+from demo_utils import plot_image, plot_gif, plt_cmp_3dobj
 
 # Set sinogram shape
 num_det_rows = 400
@@ -21,11 +21,12 @@ delta_pixel_detector = 0.45
 delta_pixel_image = 0.5
 channel_offset = 0
 row_offset = 0
+max_iterations = 20
 
 # Display parameters
 vmin = 1.0
-vmax = 1.2
-
+vmax = 1.1
+filename = 'output/3D_shepp_logan/results.png'
 
 # Generate a 3D shepp logan phantom.
 ROR, boundary_size= mbircone.cone3D.compute_img_size(num_views, num_det_rows, num_det_channels,
@@ -56,11 +57,6 @@ print('Generated phantom shape = ', np.shape(phantom))
 phantom = mbircone.cone3D.pad_roi2ror(phantom, boundary_size)
 print('Padded phantom shape = ', np.shape(phantom))
 
-# display phantom
-title = f'Slice {display_slice:d} of 3D Shepp Logan Phantom.'
-plot_image(phantom[display_slice], title=title, filename='output/3d_shepp_logan_phantom.png', vmin=vmin, vmax=vmax)
-
-
 # Generate simulated data using forward projector on the 3D shepp logan phantom.
 angles = np.linspace(0, 2 * np.pi, num_views, endpoint=False)
 
@@ -76,43 +72,20 @@ sino = mbircone.cone3D.project(phantom, angles,
 os.makedirs('output/3D_shepp_logan/', exist_ok=True)
 
 print('sino shape = ', np.shape(sino), sino.dtype)
-plot_image(sino[:, display_slice, :], title='sino', filename='output/3D_shepp_logan/sino-shepp-logan-3D-slice%d.png' % display_slice)
-plot_gif(sino, 'output', 'sino-shepp-logan-3D')
+plot_image(sino[display_view, :, :], title='sino', filename='output/3D_shepp_logan/sino-shepp-logan-3D-view(%.2f).png' % angles[0])
+# plot_gif(sino, 'output', 'sino-shepp-logan-3D')
 
 recon = mbircone.cone3D.recon(sino, angles,
                               dist_source_detector=dist_source_detector, magnification=magnification,
                               delta_pixel_detector=delta_pixel_detector, delta_pixel_image=delta_pixel_image,
                               channel_offset=channel_offset, row_offset=row_offset,
-                              sharpness=sharpness, snr_db=snr_db, max_iterations=100)
+                              sharpness=sharpness, snr_db=snr_db, max_iterations=max_iterations)
 
 print('recon shape = ', np.shape(recon))
-plot_gif(recon, 'output/3D_shepp_logan', 'recon', vmin=vmin, vmax=vmax)
-plot_gif(phantom, 'output/3D_shepp_logan', 'phantom', vmin=vmin, vmax=vmax)
+# plot_gif(recon, 'output/3D_shepp_logan', 'recon', vmin=vmin, vmax=vmax)
+# plot_gif(phantom, 'output/3D_shepp_logan', 'phantom', vmin=vmin, vmax=vmax)
+np.save('output/3D_shepp_logan/recon.npy', recon)
 
-# Compute Normalized Root Mean Squared Error
-nrmse = mbircone.phantom.nrmse(recon, phantom)
-
-# display phantom
-title = f'Slice {display_slice:d} of 3D Shepp-logan Phantom xy.'
-plot_image(phantom[display_slice], title=title, filename='output/3D_shepp_logan/3d_shepp_logan_phantom_xy.png',
-           vmin=vmin, vmax=vmax)
-title = f'Slice {display_x:d} of 3D Shepp-logan Phantom yz.'
-plot_image(phantom[:, display_x, :], title=title, filename='output/3D_shepp_logan/3d_shepp_logan_phantom_yz.png',
-           vmin=vmin, vmax=vmax)
-title = f'Slice {display_y:d} of 3D Shepp-logan Phantom xz.'
-plot_image(phantom[:, :, display_y], title=title, filename='output/3D_shepp_logan/3d_shepp_logan_phantom_xz.png',
-           vmin=vmin, vmax=vmax)
-
-
-# display reconstruction
-title = f'Slice {display_slice:d} of 3D Recon xy with NRMSE={nrmse:.3f}.'
-plot_image(recon[display_slice], title=title, filename='output/3D_shepp_logan/3d_shepp_logan_recon_xy.png', vmin=vmin,
-           vmax=vmax)
-title = f'Slice {display_y:d} of 3D Recon xz with NRMSE={nrmse:.3f}.'
-plot_image(recon[:, :, display_y], title=title, filename='output/3D_shepp_logan/3d_shepp_logan_recon_xz.png', vmin=vmin,
-           vmax=vmax)
-title = f'Slice {display_x:d} of 3D Recon yz with NRMSE={nrmse:.3f}.'
-plot_image(recon[:, display_x, :], title=title, filename='output/3D_shepp_logan/3d_shepp_logan_recon_yz.png', vmin=vmin,
-           vmax=vmax)
+plt_cmp_3dobj(phantom, recon, display_slice, display_x, display_y, vmin, vmax, filename)
 
 input("press Enter")
--- demo/demo_utils.py
+++ demo/demo_utils.py
@@ -1,23 +1,109 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 import imageio
 
 
-def plot_gif(x,save_dir,name,vmin=None,vmax=None):
-    images=[]
+def font_setting():
+    SMALL_SIZE = 8
+    MEDIUM_SIZE = 10
+    BIGGER_SIZE = 12
+    plt.rc('font', size=SMALL_SIZE)  # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)  # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)  # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)  # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
+def nrmse(image, reference_image):
+    """
+    Compute the normalized root mean square error between image and reference_image.
+    Args:
+        image: Calculated image
+        reference_image: Ground truth image
+    Returns:
+        Root mean square of (image - reference_image) divided by RMS of reference_image
+    """
+    rmse = np.sqrt(((image - reference_image) ** 2).mean())
+    denominator = np.sqrt(((reference_image) ** 2).mean())
+
+    return rmse / denominator
+
+
+def plt_cmp_3dobj(phantom, recon, display_slice=None, display_x=None, display_y=None, vmin=None, vmax=None,
+                  filename=None):
+    Nz, Nx, Ny = recon.shape
+    if display_slice is None:
+        display_slice = Nz // 2
+    if display_x is None:
+        display_x = Nx // 2
+    if display_y is None:
+        display_y = Ny // 2
+
+    # Compute Normalized Root Mean Squared Error
+    nrmse_1 = nrmse(recon, phantom)
+
+    font_setting()
+
+    # display phantom
+    fig, axs = plt.subplots(2, 3)
+
+    title = f'Phantom: Axial Scan {display_slice:d}.'
+    axs[0, 0].imshow(phantom[display_slice], vmin=vmin, vmax=vmax, cmap='gray')
+    axs[0, 0].set_title(title)
+
+    title = f'Phantom: Coronal Scan {display_x:d}.'
+    axs[0, 1].imshow(phantom[:, display_x, :], vmin=vmin, vmax=vmax, cmap='gray')
+    axs[0, 1].set_title(title)
+
+    title = f'Phantom: Sagittal Scan {display_y:d}.'
+    axs[0, 2].imshow(phantom[:, :, display_y], vmin=vmin, vmax=vmax, cmap='gray')
+    axs[0, 2].set_title(title)
+
+    # display reconstruction
+    title = f'Recon: Axial Scan {display_slice:d}.'
+    axs[1, 0].imshow(recon[display_slice], vmin=vmin, vmax=vmax, cmap='gray')
+    axs[1, 0].set_title(title)
+
+    title = f'Recon: Coronal Scan {display_y:d}.'
+    axs[1, 1].imshow(recon[:, display_x, :], vmin=vmin, vmax=vmax, cmap='gray')
+    axs[1, 1].set_title(title)
+
+    title = f'Recon: Sagittal Scan {display_x:d}.'
+    im = axs[1, 2].imshow(recon[:, :, display_y], vmin=vmin, vmax=vmax, cmap='gray')
+    axs[1, 2].set_title(title)
+
+    # Add colorbar
+    plt.subplots_adjust(wspace=0.9, hspace=0.4, right=0.8)
+    cax = plt.axes([0.85, 0.15, 0.05, 0.7])
+    plt.colorbar(im, cax=cax)
+
+    plt.suptitle(f"3D Shepp-Logan Recon Phantom VS Recon with NRMSE={nrmse_1:.3f}.")
+
+    if filename is not None:
+        try:
+            plt.savefig(filename, dpi=600)
+        except:
+            print("plot_image() Warning: Can't write to file {}".format(filename))
+
+
+def plot_gif(x, save_dir, name, vmin=None, vmax=None):
+    images = []
     for i in range(x.shape[0]):
         fig = plt.figure()
-        plt.imshow(x[i], vmin=vmin,  vmax=vmax).set_cmap('gray')
-        plt.title("Slice: %d"%(i+1))
+        plt.imshow(x[i], vmin=vmin, vmax=vmax).set_cmap('gray')
+        plt.title("Slice: %d" % (i + 1))
         plt.colorbar()
-        fig.canvas.draw() 
+        fig.canvas.draw()
         image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
-        image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+        image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
         images.append(image)
         plt.close()
-    imageio.mimsave(save_dir + "/%s.gif"%name, images, fps=5)
+    imageio.mimsave(save_dir + "/%s.gif" % name, images, fps=5)
+
+
 
 def plot_image(img, title=None, filename=None, vmin=None, vmax=None):
     """
@@ -33,7 +119,7 @@ def plot_image(img, title=None, filename=None, vmin=None, vmax=None):
 
     plt.ion()
     fig = plt.figure()
-    imgplot = plt.imshow(img, vmin=vmin,  vmax=vmax)
+    imgplot = plt.imshow(img, vmin=vmin, vmax=vmax)
     plt.title(label=title)
     imgplot.set_cmap('gray')
     plt.colorbar()
@@ -45,27 +131,10 @@ def plot_image(img, title=None, filename=None, vmin=None, vmax=None):
 
 
 def read_ND(filePath, n_dim, dtype='float32', ntype='int32'):
-
     with open(filePath, 'rb') as fileID:
-
-        sizesArray = np.fromfile( fileID, dtype=ntype, count=n_dim)
+        sizesArray = np.fromfile(fileID, dtype=ntype, count=n_dim)
         numElements = np.prod(sizesArray)
         dataArray = np.fromfile(fileID, dtype=dtype, count=numElements).reshape(sizesArray)
 
     return dataArray
 
-
-
-def nrmse(image, reference_image):
-    """
-    Compute the normalized root mean square error between image and reference_image.
-    Args:
-        image: Calculated image
-        reference_image: Ground truth image
-    Returns:
-        Root mean square of (image - reference_image) divided by RMS of reference_image
-    """
-    rmse = np.sqrt(((image - reference_image) ** 2).mean())
-    denominator = np.sqrt(((reference_image) ** 2).mean())
-
-    return rmse/denominator
