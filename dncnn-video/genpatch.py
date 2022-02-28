Code for new training mechanism (scaling with segmentation) Tested Nan bug still exists when using GPU to train

dncnn-video/genpatch.py
@@ -78,9 +78,9 @@ def getPatchList_3D_rotAll(recon3D, params):
 
     return patchList3D
 
-def get_patches_from_recon4D(fileList_recon4D, params):
+def get_patches_from_recon4D(reconList, params):
 
-    reconList = read3Dlist(fileList_recon4D, isRecon=True)
+    # reconList = read3Dlist(fileList_recon4D, isRecon=True)
 
     stack_of_patchList3D = []
     for id,recon in enumerate(reconList):
@@ -165,7 +165,6 @@ def main():
 
     patchArray_clean = np.stack(patchList_clean_good, axis=0) # n x conv1 x conv2 x chan
     patchArray_noisy = np.stack(patchList_noisy_good, axis=0) # n x conv1 x conv2 x chan
-
     print("patchArray_clean shape: {}".format(patchArray_clean.shape))
     print("patchArray_noisy shape: {}".format(patchArray_noisy.shape))
     print("saving clean patch data to ", params['paths']['patches_train_clean'])

dncnn-video/train.py
@@ -8,14 +8,13 @@ from glob import glob
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 import timeit
-
 from ruamel.yaml import YAML
-
 import cnn
 from utils_test import *
 from utils_train import *
 from utils import *
-
+from genpatch import get_patches_from_recon4D, isPatchGood
+from mbircone import preprocess
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--use_gpu', dest='use_gpu', type=int, default=1)
@@ -59,27 +58,38 @@ def partition_training_data(params):
 
 
 def main():
-
     params = get_params(args['params_path'])
     print_params(params)
-
     #os.environ['CUDA_VISIBLE_DEVICES'] = str(params['CUDA_VISIBLE_DEVICES'])
-    os.environ['CUDA_VISIBLE_DEVICES'] = ""
-
+    os.environ['CUDA_VISIBLE_DEVICES'] = "1"
     os.makedirs(os.path.dirname(params['paths']['checkpoint_dir']), exist_ok=True)
-    
     if params['use_gpu']:
         config = tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.9))
     else:
         config = tf.ConfigProto()
 
 
+    train_path = '/depot/bouman/data/share_wenrui_diyu/training/16955-2014-1-057-AT 18 inlb Y Slices'
+    train_path_list = sorted(glob(os.path.join(train_path, '*')))
+    cleanData_train = np.array([preprocess._read_scan_img(img_path) for img_path in train_path_list])
     np.random.seed(seed=1)
-    cleanData_train, cleanData_eval, noisyData_train, noisyData_eval = partition_training_data(params)
+    upper_range = calc_upper_range(cleanData_train, percentile=params['percentile'], gauss_sigma=params['gauss_sigma'], threshold=params['indicator_threshold'])
+    print("upper_range from clean data = ", upper_range)
+    cleanData_train = [cleanData_train] 
+    clean_patches_train_orig = get_patches_from_recon4D(cleanData_train, params)
+    print("shape of clean patches before inspection = ", np.shape(clean_patches_train_orig))
+    clean_patches_train = []
+    for patch_clean in clean_patches_train_orig:
+        if isPatchGood(patch_clean, params):
+            clean_patches_train.append(patch_clean)
 
+    clean_patches_train = np.stack(clean_patches_train, axis=0) # n x conv1 x conv2 x chan
+    print("shape of clean patches after inspection = ", np.shape(clean_patches_train))
+    clean_patches_train = clean_patches_train / upper_range
+    np.save(os.path.join(params['paths']['out_dir'], 'clean_patches.npy'), clean_patches_train)
     with tf.Session(config=config) as sess:
-        denoiser_obj = cnn.denoiser(sess, size_z_in=params['size_z_in'], size_z_out=params['size_z_out'], numLayers=params['numLayers'], width=params['width'], is_normalize=params['is_normalize'])
-        denoiser_obj.train(cleanData_train, cleanData_eval, noisyData_train, noisyData_eval, params)
+        denoiser_obj = cnn.denoiser(sess, size_z_in=params['size_z_in'], size_z_out=params['size_z_out'], numLayers=params['numLayers'], width=params['width'])
+        denoiser_obj.train(clean_patches_train, params)
 
 
 

dncnn-video/utils.py
@@ -7,6 +7,7 @@ import array
 import pdb
 from ruamel.yaml import YAML
 from skimage.filters import gaussian
+import time
 
 #############################################################################
 ## Semi 2D
@@ -351,7 +352,7 @@ def pythonize_params(params):
     return params
 
 
-def calc_upper_range(img, percentile=75, gauss_sigma=1.5, is_segment=True, threshold=0.5):
+def calc_upper_range(img, percentile=75, gauss_sigma=2., is_segment=True, threshold=0.5):
     '''
     Given a 4D image volume of shape (Nt, Nz, Nx, Ny), calculate the image upper range.
     '''
@@ -361,11 +362,16 @@ def calc_upper_range(img, percentile=75, gauss_sigma=1.5, is_segment=True, thres
         for _ in range(4-np.ndim(img)):
             img = np.expand_dims(img, axis=0)
     num_tp, num_axial_slices, _, _ = np.shape(img)
+    start_time = time.time()
     img_smooth = np.array([[gaussian(img[t,i,:,:], gauss_sigma, preserve_range=True) for t in range(num_tp)] for i in range(num_axial_slices)])
+    end_time = time.time()
+    time_elapsed = end_time - start_time
+    print(f"Gaussian filtering takes {time_elapsed:.2f} sec for an image of size ", img.shape)
     if is_segment:
         img_mean = np.mean(img_smooth)
         indicator = img_smooth > threshold * img_mean
     else:
         indicator = np.ones(img.shape)
     img_upper_range = np.percentile(img_smooth[indicator], percentile)
-    return img_upper_range, np.squeeze(img_smooth), np.squeeze(indicator)
+    #return img_upper_range, np.squeeze(img_smooth), np.squeeze(indicator)
+    return img_upper_range

dncnn-video/utils_train.py
@@ -19,16 +19,10 @@ from utils import *
 ## Data Augment
 #############################################################################
 
-def generateCleanNoisyPair(clean_data, noisy_data, batch_id, train_params):
-
+def generateCleanNoisyPair(clean_data, batch_id, train_params):
     clean_batch = select_batch(clean_data, batch_id, train_params['batch_size'])
-    noisy_batch = select_batch(noisy_data, batch_id, train_params['batch_size'])
-
-    clean_batch, noisy_batch = augment_batch([clean_batch, noisy_batch], train_params)
-
-    clean_batch = addNoise_batch(clean_batch, train_params['perturb_sigma'])
-    noisy_batch = addNoise_batch(noisy_batch, train_params['noise_sigma'])
-
+    clean_batch = augment_batch(clean_batch, train_params)
+    noisy_batch = addNoise_batch(clean_batch, train_params['noise_sigma'])
     clean_batch = semi2DCNN_select_z_out_from_z_in(clean_batch, train_params['size_z_in'], train_params['size_z_out'])
 
     return clean_batch, noisy_batch
@@ -55,35 +49,27 @@ def select_batch(data, batch_id, batch_size):
 
     return batch
 
-def augment_batch(batchList, train_params):
+def augment_batch(clean_batch, train_params):
 
-    batch_size = batchList[0].shape[0]
+    batch_size = clean_batch.shape[0]
     for patch_id in range(batch_size):
+        clean_batch[patch_id] = augment_patch(clean_batch[patch_id], train_params)
 
-        patchList = [ batch[patch_id] for batch in batchList ]
-
-        patchList = augment_patch(patchList, train_params)
-
-        for batch,patch in zip(batchList,patchList):
-            batch[patch_id] = patch 
-
-    return batchList
+    return clean_batch
 
 
-def augment_patch(patchList, train_params):
+def augment_patch(patch, train_params):
 
     # rotate and flip
-    if patchList[0].shape[0]!=1 and patchList[0].shape[1]!=1:
-        patchList = orient_patch(patchList, train_params['is_augOrientOn'])
+    if patch.shape[0]!=1 and patch.shape[1]!=1:
+        patch = orient_patch(patch, train_params['is_augOrientOn'])
+    if train_params['is_randShift_on']:
+        print("Adding random shift and gradient to clean patch!")
+        patch = addRandShiftAndGradient_patch(patch, train_params)
 
-    #print('Before: min: {}, max: {}'.format( batch[imgID].min(), batch[imgID].max() ) )
-    #print('dark: {}, ,bright: {}'.format(dark,bright))
-    patchList = addRandShiftAndGradient_patch(patchList, train_params)
-    #print('After : min: {}, max: {}'.format( batch[imgID].min(), batch[imgID].max() ) )
+    return patch
 
-    return patchList
-
-def orient_patch(patchList, is_augOrientOn):
+def orient_patch(patch, is_augOrientOn):
 
     if is_augOrientOn:
         is_flipud = np.random.randint(2)
@@ -96,32 +82,23 @@ def orient_patch(patchList, is_augOrientOn):
         is_flipz = 0
         is_swapxy = 0 
 
-    # print([is_flipud, is_fliplr, is_flipz, is_swapxy])
+    orig_shape = patch.shape
 
-    for i,patch in enumerate(patchList):
-
-        orig_shape = patch.shape
-
-        if is_flipud==1:
-            patch = np.flipud(patch)
-
-        if is_fliplr==1:
-            patch = np.fliplr(patch)
-
-        if is_flipz==1:
-            patch = np.flip(patch, axis=2)
-
-        if is_swapxy==1:
-            patch = np.swapaxes(patch, 0, 1)
-
-        assert patch.shape==orig_shape, 'orient_patch: shape changed'
-
-        patchList[i] = patch
-
-    return patchList
+    if is_flipud==1:
+        patch = np.flipud(patch)
+    if is_fliplr==1:
+        patch = np.fliplr(patch)
+    if is_flipz==1:
+        patch = np.flip(patch, axis=2)
+    if is_swapxy==1:
+        patch = np.swapaxes(patch, 0, 1)
+    assert patch.shape==orig_shape, 'orient_patch: shape changed'
 
 
-def addRandShiftAndGradient_patch(patchList, train_params):
+    return patch
+
+
+def addRandShiftAndGradient_patch(patch, train_params):
     ''' Shifts 0 to randShift_dark and 1 to randShift_bright
         Applies gradients with range [-randGrad,randGrad] to dark and bright
         Each shifts and radius values are picked randomly and independently.
@@ -131,18 +108,14 @@ def addRandShiftAndGradient_patch(patchList, train_params):
     randGrad_dark =     np.random.uniform(low=train_params['randGrad_dark_lo'],      high=train_params['randGrad_dark_hi'])
     randGrad_bright =   np.random.uniform(low=train_params['randGrad_bright_lo'],    high=train_params['randGrad_bright_hi'])
 
-    sizes = np.array(patchList[0].shape)
+    sizes = np.array(patch.shape)
     randomGradient_dark = randomGradient(sizes)
     randomGradient_bright = randomGradient(sizes)
 
-    for i,patch in enumerate(patchList):
-        
-        patch =   randShift_dark*(1-patch) + randShift_bright*patch \
-                + randGrad_dark*(1-patch)*randomGradient_dark + randGrad_bright*patch*randomGradient_bright 
+    patch = randShift_dark*(1-patch) + randShift_bright*patch \
+            + randGrad_dark*(1-patch)*randomGradient_dark + randGrad_bright*patch*randomGradient_bright 
 
-        patchList[i] = patch
-
-    return patchList
+    return patch
 
 def randomGradient(sizes):
     ''' sizes must be 3D. output is strictly [-1,1] '''
@@ -167,15 +140,10 @@ def randomGradient(sizes):
     x = 2*x - 1
     return x
 
-def addNoise_batch(clean_batch, noise_sigma, upper_range):
+def addNoise_batch(clean_batch, noise_sigma):
 
-    noise_sigma_adjusted = noise_sigma*upper_range
-    print("Adding AWGN with sigma = ", noise_sigma_adjusted)
-    noise = np.random.normal(scale=noise_sigma*upper_range, size=clean_batch.shape)
-    # noise_sd = np.sqrt((noise**2).mean())
-    # print("gen_batch_noisy: noise sigma: {}".format(noise_sd))
-    # print("gen_batch_noisy: noise min: {}".format(noise.min()))
-    # print("gen_batch_noisy: noise max: {}".format(noise.max()))
+    print("Adding AWGN with sigma = ", noise_sigma)
+    noise = np.random.normal(scale=noise_sigma, size=clean_batch.shape)
     noisy_batch = clean_batch + noise
     
     return noisy_batch