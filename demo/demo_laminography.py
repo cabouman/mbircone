f5a41709e4c1e26a6610da85c3565e67b8b8e7a2
bommaritom <bommaritom@carleton.edu>
2023-02-16 11:00:13 -0500

    update demo_laminography.py

--- demo/demo_laminography.py
+++ demo/demo_laminography.py
@@ -27,23 +27,20 @@ num_det_channels = 64
 num_det_rows = 64
 
 # number of projection views
-num_views = 128
+num_views = 64
 
 # Phantom parameters
-num_slices_phantom = 16
-num_rows_phantom = 64
-num_cols_phantom = 64
+num_phantom_slices = 16
+num_phantom_rows = 64
+num_phantom_cols = 64
 
 # Reconstruction parameters
-num_slices_image = 16
-num_rows_image = 146
-num_cols_image = 146
+num_image_slices = 16
+num_image_rows = 146
+num_image_cols = 146
 
-# Size of constant padding around phantom as a multiple of num_rows_phantom or num_cols_phantom
+# Size of constant padding around phantom as a multiple of num_phantom_rows or num_phantom_cols
 pad_factor = 2
-
-# Reconstruction size
-num_image_slices = 18
 
 # qGGMRF recon parameters
 sharpness = 0.0                    # Controls regularization level of reconstruction by controlling prior term weighting
@@ -66,12 +63,12 @@ os.makedirs(save_path, exist_ok=True)
 ######################################################################################
 
 print('Generating a laminography phantom ...')
-phantom = mbircone.phantom.gen_lamino_sample_3d(num_rows_phantom, num_cols_phantom,
-                                                num_slices_phantom, pad_factor=pad_factor)
+phantom = mbircone.phantom.gen_lamino_sample_3d(num_phantom_rows, num_phantom_cols,
+                                                num_phantom_slices, pad_factor=pad_factor)
 
 # Scale the phantom by a factor of 10.0 to make the projections physical realistic -log attenuation values
 phantom = phantom/10.0
-print('Phantom shape is:', num_slices_phantom, num_rows_phantom, num_cols_phantom)
+print('Phantom shape is:', num_phantom_slices, num_phantom_rows, num_phantom_cols)
 
 
 ######################################################################################
@@ -91,9 +88,9 @@ print('Synthetic sinogram shape: (num_views, num_det_rows, num_det_channels) = '
 print('Performing 3D qGGMRF reconstruction ...')
 
 recon = mbircone.laminography.recon_lamino(sino, angles, theta_radians,
-                                           num_image_rows=146,
-                                           num_image_cols=146,
-                                           num_image_slices=16,
+                                           num_image_rows=num_image_rows,
+                                           num_image_cols=num_image_cols,
+                                           num_image_slices=num_image_slices,
                                            sharpness=sharpness, snr_db=snr_db)
 
 print('recon shape = ', np.shape(recon))
@@ -124,22 +121,22 @@ plot_image(np.abs(sino[0,:,:]-sino[num_views//3,:,:]),
 display_phantom = phantom
 display_recon = recon
 
-(num_slices_image, num_rows_image, num_cols_image) = np.shape(recon)
+(num_image_slices, num_image_rows, num_image_cols) = np.shape(recon)
 
 # Determine where the relevant phantom region begins and ends
-recon_row_start = int((num_rows_image-num_rows_phantom)/2)
-recon_row_end = recon_row_start + num_rows_phantom
-recon_col_start = int((num_cols_image-num_cols_phantom)/2)
-recon_col_end = recon_col_start + num_cols_phantom
-recon_slice_start = int((num_slices_image-num_slices_phantom)/2)
-recon_slice_end = recon_slice_start + num_slices_phantom
+recon_row_start = int((num_image_rows-num_phantom_rows)/2)
+recon_row_end = recon_row_start + num_phantom_rows
+recon_col_start = int((num_image_cols-num_phantom_cols)/2)
+recon_col_end = recon_col_start + num_phantom_cols
+recon_slice_start = int((num_image_slices-num_phantom_slices)/2)
+recon_slice_end = recon_slice_start + num_phantom_slices
 recon_area_of_interest = recon[recon_slice_start:recon_slice_end, recon_row_start:recon_row_end,
                          recon_col_start:recon_col_end]
 
-phantom_row_start = int(num_rows_phantom * pad_factor)
-phantom_row_end = phantom_row_start + num_rows_phantom
-phantom_col_start = int(num_cols_phantom * pad_factor)
-phantom_col_end = phantom_col_start + num_cols_phantom
+phantom_row_start = int(num_phantom_rows * pad_factor)
+phantom_row_end = phantom_row_start + num_phantom_rows
+phantom_col_start = int(num_phantom_cols * pad_factor)
+phantom_col_end = phantom_col_start + num_phantom_cols
 phantom_area_of_interest = phantom[:, phantom_row_start:phantom_row_end, phantom_col_start:phantom_col_end]
 
 # Compute and display reconstruction error in phantom region
