797fc84d4f6483aa68641310008250944761630d
bommaritom <bommaritom@carleton.edu>
2023-03-21 14:03:01 -0400

    remove cylinder ROI and color map from demo_laminography.py

--- demo/demo_laminography.py
+++ demo/demo_laminography.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import mbircone
-from demo_utils import plot_image, nrmse, create_circular_mask
+from demo_utils import plot_image, nrmse
 
 """
 This script is a demonstration of the laminography reconstruction algorithm. Demo functionality includes
@@ -114,22 +114,22 @@ display_y_recon = recon.shape[2] // 2
 
 # phantom images
 plot_image(phantom[display_slice_phantom], title=f'phantom, axial slice {display_slice_phantom}',
-           filename=os.path.join(save_path, 'phantom_axial.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'phantom_axial.png'), vmin=vmin, vmax=vmax)
 plot_image(phantom[:,display_x_phantom,:], title=f'phantom, coronal slice {display_x_phantom}',
-           filename=os.path.join(save_path, 'phantom_coronal.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'phantom_coronal.png'), vmin=vmin, vmax=vmax)
 plot_image(phantom[:,:,display_y_phantom], title=f'phantom, sagittal slice {display_y_phantom}',
-           filename=os.path.join(save_path, 'phantom_sagittal.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'phantom_sagittal.png'), vmin=vmin, vmax=vmax)
 
 # recon images
 plot_image(recon[display_slice_recon], title=f'qGGMRF recon, axial slice {display_slice_recon}, '
                                                      f'Θ='+str(theta_degrees)+' degrees',
-           filename=os.path.join(save_path, 'recon_axial.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'recon_axial.png'), vmin=vmin, vmax=vmax)
 plot_image(recon[:, display_x_recon,:], title=f'qGGMRF recon, coronal slice {display_x_recon}, '
                                                       f'Θ='+str(theta_degrees)+' degrees',
-           filename=os.path.join(save_path, 'recon_coronal.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'recon_coronal.png'), vmin=vmin, vmax=vmax)
 plot_image(recon[:, :, display_y_recon], title=f'qGGMRF recon, sagittal slice {display_y_recon}, '
                                                        f'Θ='+str(theta_degrees)+' degrees',
-           filename=os.path.join(save_path, 'recon_sagittal.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'recon_sagittal.png'), vmin=vmin, vmax=vmax)
 
 #####################################################################################
 # Generate NRMSE and error images
@@ -144,7 +144,6 @@ phantom_row_start = int(num_phantom_rows * (tile_rows-1)/2)
 phantom_row_end = phantom_row_start + num_phantom_rows
 phantom_col_start = int(num_phantom_cols * (tile_cols-1)/2)
 phantom_col_end = phantom_col_start + num_phantom_cols
-phantom_roi = phantom[:, phantom_row_start:phantom_row_end, phantom_col_start:phantom_col_end]
 
 # Determine where the relevant recon region begins and ends
 recon_row_start = int((num_image_rows-num_phantom_rows)/2)
@@ -153,25 +152,19 @@ recon_col_start = int((num_image_cols-num_phantom_cols)/2)
 recon_col_end = recon_col_start + num_phantom_cols
 recon_slice_start = int((num_image_slices-num_phantom_slices)/2)
 recon_slice_end = recon_slice_start + num_phantom_slices
+
+phantom_roi = phantom[:, phantom_row_start:phantom_row_end, phantom_col_start:phantom_col_end]
 recon_roi = recon[recon_slice_start:recon_slice_end, recon_row_start:recon_row_end,
                   recon_col_start:recon_col_end]
 
-# Compute and display reconstruction error in cylindrical phantom region
+# Compute and display reconstruction error
 
-# Create cylindrical mask
-mask = create_circular_mask(num_phantom_rows, num_phantom_cols)
-mask = np.tile(mask, (num_phantom_slices, 1, 1))
-
-# Create masked regions of interest
-masked_recon_roi = np.ma.masked_where(mask, recon_roi)
-masked_phantom_roi = np.ma.masked_where(mask, phantom_roi)
-
-nrmse = nrmse(masked_recon_roi, masked_phantom_roi)
+nrmse = nrmse(recon_roi, phantom_roi)
 print(f'qGGMRF normalized rms reconstruction error within laminography phantom window of diameter 64: '
       f'{nrmse:.3g}')
 
 # Generate image representing error
-error = np.abs(masked_recon_roi - masked_phantom_roi)
+error = np.abs(recon_roi - phantom_roi)
 
 display_slice_error = error.shape[0] // 4
 display_x_error = error.shape[1] // 2
@@ -180,13 +173,13 @@ display_y_error = error.shape[2] // 2
 # error images
 plot_image(error[display_slice_error], title=f'error, axial slice {display_slice_error}, '
                                                      f'Θ='+str(theta_degrees)+' degrees',
-           filename=os.path.join(save_path, 'error_axial.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'error_axial.png'), vmin=vmin, vmax=vmax)
 plot_image(error[:, display_x_error,:], title=f'error, coronal slice {display_x_error}, '
                                                       f'Θ='+str(theta_degrees)+' degrees',
-           filename=os.path.join(save_path, 'error_coronal.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'error_coronal.png'), vmin=vmin, vmax=vmax)
 plot_image(error[:, :, display_y_error], title=f'error, sagittal slice {display_y_error}, '
                                                        f'Θ='+str(theta_degrees)+' degrees',
-           filename=os.path.join(save_path, 'error_sagittal.png'), vmin=vmin, vmax=vmax, cmap='viridis')
+           filename=os.path.join(save_path, 'error_sagittal.png'), vmin=vmin, vmax=vmax)
 
 print(f"Images saved to {save_path}.")
 input("Press Enter")
